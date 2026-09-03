85bd94af3867a0a370b841060c673e0493ccfee2
EduardoNeville <eduardoneville82@gmail.com>
2026-09-03 15:41:56 +0200

    fix(blog): import firestoreDb in blog pages

--- app/src/pages/BlogListPage.tsx
+++ app/src/pages/BlogListPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Container } from '../components/ui/Container'
 import { CtaBand } from '../components/shared/CtaBand'
-import { getApp } from '../lib/firebase'
+import { getApp, firestoreDb } from '../lib/firebase'
 import type { Post } from '../lib/posts'
 import ctaCoast from '../assets/cta-coast.jpg'
 
